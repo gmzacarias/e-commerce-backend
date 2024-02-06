4da88b470051cb9918a0d856ea3fe92af9e8e4ee
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2024-02-06 10:27:29 -0300

    se actualizo el readme

--- pages/api/ipn/mercadopago.ts
+++ pages/api/ipn/mercadopago.ts
@@ -1,13 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import method from "micro-method-router"
 import { updateStatusOrder } from "controllers/order"
-import { validateIpnMercadoPago } from "lib/schemaMiddleware"
 
 async function postHandler(req: NextApiRequest, res: NextApiResponse) {
     const { topic, id } = req.query as any
-    // console.log(topic, id)
     try {
-        // await validateIpnMercadoPago(req, res)
         await updateStatusOrder(topic, id)
         res.send({ message: "Compra exitosa" })
     }

b9f5ecdeecc87c4a18c6bf9aad14a6ddad0aa465
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2024-02-14 01:30:41 -0300

    se agrego localhost al cors

--- lib/cors.ts
+++ lib/cors.ts
@@ -1,20 +1,31 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Cors from 'cors';
 
-  const cors = Cors({
-    origin: "*",
-    methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS", "HEAD"],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+if (process.env.NODE_ENV == "development") {
+  var origin = "http://localhost:3000";
 
-  export function runMiddleware(req: NextApiRequest, res: NextApiResponse) {
-    return new Promise((resolve, reject) => {
-      cors(req, res, (result) => {
-        if (result instanceof Error) {
-          return reject(result);
-        }
-        return resolve(result);
-      });
+}
+ else if (process.env.NODE_ENV == "production") {
+  var origin = "*";
+}
+
+
+
+const cors = Cors({
+  origin: origin,
+  methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS", "HEAD"],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+});
+
+export function runMiddleware(req: NextApiRequest, res: NextApiResponse) {
+  return new Promise((resolve, reject) => {
+    cors(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      console.log(cors.origin)
+      return resolve(result);
     });
-  }
+  });
+}

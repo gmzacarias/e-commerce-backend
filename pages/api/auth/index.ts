3519fe580216eb88f6d4d6ead8dcfcc5f5448d9c
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2024-02-13 23:44:32 -0300

    probando cors

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -1,17 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import method from "micro-method-router"
-import NextCors from "nextjs-cors"
+// import NextCors from "nextjs-cors"
 import { sendCode } from "controllers/auth"
 import { validateAuth } from "lib/schemaMiddleware"
 
 async function postHandler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        await NextCors(req, res, {
-            // Options
-            methods: ['POST'],
-            origin: '*',
-            optionsSuccessStatus: 200,
-         });
+        // await NextCors(req, res, {
+        //     // Options
+        //     methods: ['POST'],
+        //     origin: '*',
+        //     optionsSuccessStatus: 200,
+        //  });
         await validateAuth(req, res)
         if (res.statusCode === 403) {
             throw new Error("No se envio el mail")

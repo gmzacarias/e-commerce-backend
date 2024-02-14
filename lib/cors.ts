326757f351ff8e79c75e181f364aac3cdbef775d
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2024-02-14 13:37:48 -0300

    probando cors

--- lib/cors.ts
+++ lib/cors.ts
@@ -1,20 +1,29 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import Cors from 'cors';
+import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
 
-const cors = Cors({
-  origin: ['http://localhost:3000', '*'], // Permitir solicitudes desde localhost y cualquier origen en producción,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+export const cors = Cors({
+  methods: ["POST", "GET", "HEAD", "PATCH", "DELETE", "PUT"],
+  origin: ["http://localhost:3001/api","*"]
 });
 
-export function runMiddleware(req: NextApiRequest, res: NextApiResponse) {
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+export function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function,
+) {
   return new Promise((resolve, reject) => {
-    cors(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
+    fn(req, res, (result: any) => {
+      cors(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        fn(req, res)
+        return resolve(result);
+      })
     });
   });
 }
--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -1,13 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import method from "micro-method-router"
-import { runMiddleware } from "lib/cors"
+import { handlerCORS } from "lib/corsMiddleware"
 import { sendCode } from "controllers/auth"
 import { validateAuth } from "lib/schemaMiddleware"
 
 async function postHandler(req: NextApiRequest, res: NextApiResponse) {
     try {
-
-        await runMiddleware(req, res);
         await validateAuth(req, res)
         if (res.statusCode === 403) {
             throw new Error("No se envio el mail")
@@ -24,4 +22,4 @@ const handler = method({
     post: postHandler
 })
 
-export default handler
+export default handlerCORS(handler)

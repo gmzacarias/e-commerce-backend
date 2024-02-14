a433eef0acfa5027debf1c01e49554da7ed548f0
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2024-02-14 00:20:24 -0300

    probando cors

--- lib/cors.ts
+++ lib/cors.ts
@@ -2,10 +2,11 @@ import Cors from 'cors';
 
 // Inicializa el middleware CORS con las opciones deseadas
 const corsOptions = {
-  origin: 'http://tu-dominio-permitido.com',  // Reemplaza con tu dominio permitido
-  methods: ['GET', 'POST', 'OPTIONS'],         // Métodos permitidos
+  origin: "*",  // Reemplaza con tu dominio permitido
+  methods: ["GET", "POST","PUT","PATCH","DELETE","OPTIONS"],         // Métodos permitidos
   allowedHeaders: ['Content-Type'],            // Encabezados permitidos
 };
 
 // Inicializa el middleware CORS con las opciones
 export const cors = Cors(corsOptions)
+console.log(cors)

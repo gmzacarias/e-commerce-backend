5b7c135676d8876628f4662fadcfd734e8fa115e
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-04-03 04:35:15 -0300

    chore - se eliminaron comentarios innecesarios en handlerCORS

--- lib/corsMiddleware.ts
+++ lib/corsMiddleware.ts
@@ -1,19 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import NextCors from "nextjs-cors";
 
-export function handlerCORS(callback) {
+export function handlerCORS(callback: { (req: NextApiRequest, res: NextApiResponse): void; (req: NextApiRequest, res: NextApiResponse): void; (req: NextApiRequest, res: NextApiResponse): void; (req: NextApiRequest, res: NextApiResponse): void; (req: NextApiRequest, res: NextApiResponse): void; (req: NextApiRequest, res: NextApiResponse): void; (arg0: NextApiRequest, arg1: NextApiResponse): void; }) {
     return async function (req: NextApiRequest, res: NextApiResponse) {
-      // Run the cors middleware
-      // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
       await NextCors(req, res, {
-        // Options
         methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
         origin: ["*","http://localhost:3000","https://localhost:3000","https://e-commerce-smartshop.vercel.app"],
-        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-      });
-  
-      // Rest of the API logic
+        optionsSuccessStatus: 200,
+      })
       callback(req, res);
-      //res.json({ message: "Hello NextJs Cors!" });
     }
 }

19e6a85a8b4ba0777b6c54be398cfe4728c27855
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-04-05 01:46:13 -0300

    refactor - se corrigio el tipo de request ,modificandolo por nextApiRequest

--- utils/pagination.ts
+++ utils/pagination.ts
@@ -1,8 +1,8 @@
-import { NextRequest } from "next/server"
+import { NextApiRequest } from "next"
 
-export function getOffsetAndLimit(req: NextRequest, maxLimit = 100, maxOffset = 10000) {
-    const queryLimit = parseInt(req.nextUrl.searchParams.get("limit") ?? "10", 10)
-    const queryOffset = parseInt(req.nextUrl.searchParams.get("offset") ?? "0", 10)
+export function getOffsetAndLimit(req: NextApiRequest, maxLimit = 100, maxOffset = 10000) {
+    const queryLimit = parseInt(req.query.limit as string ?? "10", 10)
+    const queryOffset = parseInt(req.query.offset as string ?? "0", 10)
     const limit = Math.min(Math.max(queryLimit, 1), maxLimit)
     const offset = Math.min(queryOffset, queryLimit)
     return { limit, offset }

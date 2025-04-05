785d4d8c4873ebef7d4e141a3d3a9e31e0f9db79
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-04-04 23:37:35 -0300

    refactor - se añadio el metodo getOrderDoc, para obtener la orden asociada a la orderId que recibe como parametro, y se modifico createNewOrder ,añadiendole la respuesta en caso de un error

--- models/order.ts
+++ models/order.ts
@@ -1,23 +1,12 @@
-
-import { firestore} from "../lib/firestore"
-
-interface OrderData {
-    userId: string,
-    products: [string],
-    status: string,
-    totalPrice: number,
-    additionalInfo: string,
-    url: string,
-    id:string,
-    created:Date
-}
+import { firestore } from "lib/firestore"
 
 const collection = firestore.collection("orders")
+
 export class Order {
     ref: FirebaseFirestore.DocumentReference
     data: OrderData
     id: string
-    constructor(id) {
+    constructor(id: string) {
         this.id = id
         this.ref = collection.doc(id)
     }
@@ -31,67 +20,76 @@ export class Order {
         this.ref.update(this.data as Record<string, any>)
     }
 
-    static async createNewOrder(newOrderData = {}) {
-        const newOrderSnap = await collection.add(newOrderData)
-        const newOrder = new Order(newOrderSnap.id)
-        newOrder.data = newOrderData as OrderData
-        return newOrder
+    static async getOrderDoc(orderId: string): Promise<{ id: string; data: OrderData }> {
+        try {
+            const orderDoc = await collection.doc(orderId).get()
+            if (!orderDoc.exists) {
+                throw new Error("no existe un documento relacionado a este usuario")
+            }
+            return {
+                id: orderDoc.id,
+                data: orderDoc.data() as OrderData
+            }
+        } catch (error) {
+            console.error(`No se pudo obtener el documento: ${error.message}`);
+            throw error
+        }
+    }
+
+    static async createNewOrder(data: OrderData): Promise<Order> {
+        try {
+            const newOrderSnap = await collection.add(data)
+            const newOrder = new Order(newOrderSnap.id)
+            newOrder.data = data
+            return newOrder
+        } catch (error) {
+            console.error(`No se pudo crear la orden: ${error.message}`);
+            throw error
+        }
     }
 
     static async getOrders(userId: string): Promise<Array<Order>> {
         try {
-            console.log(userId)
             const ordersSnap = await collection.where('userId', '==', userId).get();
             if (ordersSnap.empty) {
                 throw new Error("No existen órdenes para este usuario");
             }
             const ordersData = ordersSnap.docs.map(doc => {
-                const order = new Order(doc.id) as any
+                const order = new Order(doc.id)
                 order.data = doc.data() as OrderData
-                return order.data
+                return order
             })
-            // console.log("list", ordersData)
-            return ordersData;
+            return ordersData
         } catch (error) {
-            console.error("Error al obtener orderes del usuario: ", error.message);
-            return []
+            console.error("Error al obtener orderes del usuario:", error.message);
+            throw error
         }
     }
 
     static async getOrderById(orderId: string): Promise<Order> {
         try {
-            console.log(orderId)
-            const order = await collection.doc(orderId).get()
-            if (order.exists) {
-                const orderFound = new Order(order.id) as any
-                orderFound.data = order.data() as OrderData
-                return orderFound.data
-            } else {
-                throw new Error("No existen órdenes para este usuario");
-            }
+            const { id, data } = await this.getOrderDoc(orderId)
+            const orderFound = new Order(id)
+            orderFound.data = data
+            return orderFound
         } catch (error) {
-            console.error("Error al obtener orderes del usuario: ", error.message);
-            return null
+            console.error("Error al obtener orderes del usuario:", error.message)
+            throw error
         }
     }
 
     static async setOrderIdAndUrl(orderId: string, url: string): Promise<Order> {
         try {
-            console.log(orderId)
-            const order = await collection.doc(orderId).get()
-            if (order.exists) {
-                const orderFound = new Order(order.id)
-                orderFound.data = order.data() as OrderData
-                // console.log("mercadopago", url)
-                orderFound.data.url = url
-                orderFound.data.id = orderId
-                await orderFound.push()
-                // console.log("url guardada", url)
-                return orderFound
-            }
-
+            const { id, data } = await this.getOrderDoc(orderId)
+            const orderFound = new Order(id)
+            orderFound.data = data
+            orderFound.data.url = url
+            orderFound.data.id = orderId
+            await orderFound.push()
+            return orderFound
         } catch (error) {
-
+            console.error("Error al guardar los datos en las ordenes del usuario:", error.message)
+            throw error
         }
     }
 

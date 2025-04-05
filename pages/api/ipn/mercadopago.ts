b11abf1996f381d756d2d83f7a95447a5e3737a6
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-04-05 01:57:03 -0300

    refactor - se añadio la validacion por token en el endpoint

--- pages/api/ipn/mercadopago.ts
+++ pages/api/ipn/mercadopago.ts
@@ -2,12 +2,16 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import method from "micro-method-router"
 import { handlerCORS } from "lib/corsMiddleware"
 import { handlePaidMerchantOrder } from "controllers/order"
+import { authMiddleware } from "lib/middleware"
 
-async function postHandler(req: NextApiRequest, res: NextApiResponse) {
+async function postHandler(req: NextApiRequest, res: NextApiResponse,token:{userId:string}) {
     const topic = req.query.topic as string
     const id = req.query.id as string
     try {
-        await handlePaidMerchantOrder(topic, id)
+        if (!token) {
+            throw new Error("no hay token")
+        }
+        await handlePaidMerchantOrder(token.userId,topic, id)
         res.send({ message: "Compra exitosa" })
     } catch (error) {
         if (error.message) {
@@ -22,4 +26,6 @@ const handler = method({
     post: postHandler
 })
 
-export default handlerCORS(handler)
+const authHandler = authMiddleware(handler)
+
+export default handlerCORS(authHandler)

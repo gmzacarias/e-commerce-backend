6ea2ac704e9b137c407e51c31664c1255cdaab5c
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-04-05 21:39:36 -0300

    feat - se creo el metodo deleteHandler para eliminar los documentos asociados a una orderId, que pertenecen a un usuario especifico

--- pages/api/order/myorders/[orderId].ts
+++ pages/api/order/myorders/[orderId].ts
@@ -3,7 +3,7 @@ import method from "micro-method-router"
 import { handlerCORS } from "lib/corsMiddleware"
 import { authMiddleware } from "lib/middleware"
 import { validateQueryFindOrder } from "lib/schemaMiddleware"
-import { getOrderDataById } from "controllers/order"
+import { deleteOrderById, getOrderDataById } from "controllers/order"
 
 async function getHandler(req: NextApiRequest, res: NextApiResponse, token: { userId: string }) {
     const orderId = req.query.orderId as string
@@ -23,8 +23,27 @@ async function getHandler(req: NextApiRequest, res: NextApiResponse, token: { us
     }
 }
 
+async function deleteHandler(req: NextApiRequest, res: NextApiResponse, token: { userId: string }) {
+    const orderId = req.query.orderId as string
+    try {
+        if (!token) {
+            res.status(401).send({ message: "No hay token" })
+        }
+        await validateQueryFindOrder(req, res)
+        const deleteDocumentOrder = await deleteOrderById(token.userId, orderId)
+        res.status(200).send({ message: `orden ${orderId} eliminada correctamente`, delete: deleteDocumentOrder })
+    } catch (error) {
+        if (error.message) {
+            res.status(400).send({ message: error.message })
+        } else {
+            res.status(500).send({ message: "Error interno del servidor", error: error })
+        }
+    }
+}
+
 const handler = method({
-    get: getHandler
+    get: getHandler,
+    delete: deleteHandler,
 })
 
 const authHandler = authMiddleware(handler)

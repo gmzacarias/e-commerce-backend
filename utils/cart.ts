493c3ba9da9e034df8c9912ed27a165dab78410c
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-05-18 01:30:13 -0300

    feat - se creo el archivo cart en la carpeta utils para manejar la logica de hasProducts,calcTotalPrice,formatProductsForOrder y formatItemsForPreference desde su propio archivo

---
+++ utils/cart.ts
@@ -0,0 +1,64 @@
+function hasProducts(data: ProductData[]): boolean {
+    return data.length > 0
+}
+
+export function calcTotalPrice(data: ProductData[]): number {
+    try {
+        if (!hasProducts(data)) {
+            throw new Error("no hay productos")
+        }
+        const totalPrice = data.reduce((total, product) => {
+            return total + (product.price) * product.quantity
+        }, 0)
+        return totalPrice
+    } catch (error) {
+        console.error(error.message)
+        throw error
+    }
+}
+
+export function formatProductsForOrder(data: ProductData[]): ProductsOrder[] {
+    try {
+        if (!hasProducts(data)) {
+            throw new Error("no hay productos")
+        }
+        const products = data.map(item => {
+            return {
+                productId: item.objectID,
+                brand: item.brand,
+                model: item.model,
+                colour: item.colour,
+                photo: item.photo,
+                quantity: item.quantity
+            }
+        })
+        return products
+    } catch (error) {
+        console.error(error.message)
+        throw error
+    }
+}
+
+export async function formatItemsForPreference(data: ProductData[]): Promise<ItemsData[]> {
+    try {
+        if (!hasProducts(data)) {
+            throw new Error("no hay productos")
+        }
+        const items = data.map(item => {
+            return {
+                id: `${item.id}`,
+                title: `${item.brand} ${item.model}`,
+                description: `smartphone ${item.brand} ${item.model} `,
+                picture_url: item.photo,
+                category_id: "Phones",
+                quantity: item.quantity,
+                currency_id: "ARS",
+                unit_price: item.price
+            }
+        })
+        return items
+    } catch (error) {
+        console.error(error.message)
+        throw error
+    }
+}

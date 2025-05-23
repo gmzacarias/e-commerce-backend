d9e4891bec91badc2506ccdf74e0868794a050a1
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-05-23 17:53:38 -0300

    feat - se creo la funcion getBaseUrl, para poder manejar la logica de las url,dentro del archivo propio de Ngrok

--- services/ngrok.ts
+++ services/ngrok.ts
@@ -1,4 +1,4 @@
-export async function getNgrokUrl(): Promise<string> {
+async function getNgrokUrl(): Promise<string> {
     try {
         const res = await fetch("http://127.0.0.1:4040/api/tunnels")
         if (!res.ok) {
@@ -9,6 +9,22 @@ export async function getNgrokUrl(): Promise<string> {
         return tunnel.public_url || ""
     } catch (error) {
         console.error("error al obtener la url de Ngrok:", error.message)
+        throw error
+    }
+}
+
+async function getBaseUrl(orderId: string): Promise<UrlData> {
+    try {
+        const currentNgrokUrl = await getNgrokUrl()
+        const isDev = process.env.NODE_ENV === "development"
+        const notificationUrl = isDev ? `${currentNgrokUrl}/api/ipn/mercadopago` : "https://e-commerce-backend-lake.vercel.app/api/ipn/mercadopago"
+        const baseFrontEndUrl = isDev ? `${currentNgrokUrl}` : "https://e-commerce-smartshop.vercel.app"
+        const successUrl = `${baseFrontEndUrl}/checkoutStatus/${orderId}?status=success`
+        const pendingUrl = `${baseFrontEndUrl}/checkoutStatus/${orderId}?status=pending`
+        const failureUrl = `${baseFrontEndUrl}/checkoutStatus/${orderId}?status=failure`
+        return { notificationUrl, successUrl, pendingUrl, failureUrl }
+    } catch (error) {
+        console.error(error.message)
         throw error
     }
 }

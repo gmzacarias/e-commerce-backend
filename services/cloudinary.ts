535158ee91571c14e87ea5057630674e38887e19
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-05-27 02:14:14 -0300

    fix - se añadio el type UploadApiResponse a la funcion uploadCloudinary

--- services/cloudinary.ts
+++ services/cloudinary.ts
@@ -1,6 +1,7 @@
 import { cloudinary } from "lib/cloudinary"
+import type { UploadApiResponse } from "cloudinary"
 
-export async function uploadCloudinary(image: string) {
+export async function uploadCloudinary(image: string): Promise<UploadApiResponse> {
     try {
         const response = await cloudinary.uploader.upload(image,
             {
@@ -13,7 +14,7 @@ export async function uploadCloudinary(image: string) {
         }
         return response
     } catch (error) {
-        console.error("Error en Clodinary:", error.message)
+        console.error("Error en Cloudinary:", error.message)
         throw error
     }
 }

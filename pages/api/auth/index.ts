d06bad0c5b9bc681f6a411aeac7d65eb2ff708ff
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-05-26 21:18:23 -0300

    fix - se modifico la validacion del body,usando la funcion validateAuth y validateAuthToken que verifica los datos con el schema generado en zod

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -2,17 +2,17 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import method from "micro-method-router"
 import { handlerCORS } from "lib/corsMiddleware"
 import { sendCode } from "controllers/auth"
-import { validateAuth } from "lib/schemaMiddleware"
+import { validateAuth } from "services/validators"
 
 async function postHandler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        await validateAuth(req, res)
-        const { email } = req.body
+        const authSchema = validateAuth(req.body)
+        const { email } = authSchema
         await sendCode(email)
         return res.status(200).send({ message: "Codigo Enviado" })
     } catch (error) {
         if (error.message) {
-            res.status(401).send({ message: error.message })
+            res.status(400).send({ message: error.message })
         } else {
             res.status(500).send({ message: "Error interno del servidor", error: error })
         }
--- pages/api/auth/token.ts
+++ pages/api/auth/token.ts
@@ -2,12 +2,12 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import method from "micro-method-router"
 import { handlerCORS } from "lib/corsMiddleware"
 import { signIn } from "controllers/auth"
-import { validateAuthToken } from "lib/schemaMiddleware"
+import { validateAuthToken } from "services/validators"
 
 async function postHandler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        await validateAuthToken(req, res)
-        const { email, code } = req.body
+        const tokenSchema = validateAuthToken(req.body)
+        const { email, code } = tokenSchema
         const token = await signIn(email, code)
         res.status(200).send({ message: "inicio de sesion correcto", token: token })
     } catch (error) {

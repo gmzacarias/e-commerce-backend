9d114bcdb41248e713b1159ec8d7cc3825ebc8d0
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-05-27 00:35:51 -0300

    fix - se añadio el tipo de dato JwtPayload a la funcion signIn para mejorar el tipo de respuesta del metodo authenticate

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,15 +1,16 @@
 import { AuthService } from "services/auth"
 import { AuthRepository } from "repositories/authRepository"
 import { UserRepository } from "repositories/userRepository"
+import type { JwtPayload } from "jsonwebtoken"
 
 const authRepo = new AuthRepository()
 const userRepo = new UserRepository()
 const authService = new AuthService(authRepo, userRepo)
 
 export async function sendCode(email: string): Promise<boolean> {
-    return await authService.SendCode(email)
+    return await authService.sendCode(email)
 }
 
-export async function signIn(email: string, code: number): Promise<string> {
-    return await authService.Authenticate(email, code)
+export async function signIn(email: string, code: number): Promise<string|JwtPayload> {
+    return await authService.authenticate(email, code)
 }

394789b7d334d1bada8f967816137d6ff1eafe14
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-05-27 02:31:23 -0300

    fix - se añadieron los types que retornan las funciones getMerchantOrderId,createPreference y getPayment de mercado pago

--- services/mercadopago.ts
+++ services/mercadopago.ts
@@ -1,9 +1,12 @@
 import type { MerchantOrderGetData } from "mercadopago/dist/clients/merchantOrder/get/types"
-import { PaymentGetData } from "mercadopago/dist/clients/payment/get/types"
-import { PreferenceCreateData } from "mercadopago/dist/clients/preference/create/types"
+import type { MerchantOrderResponse } from "mercadopago/dist/clients/merchantOrder/commonTypes"
+import type { PreferenceCreateData } from "mercadopago/dist/clients/preference/create/types"
+import type { PaymentGetData } from "mercadopago/dist/clients/payment/get/types"
+import type { PreferenceResponse } from "mercadopago/dist/clients/preference/commonTypes"
+import type {PaymentResponse} from "mercadopago/dist/clients/payment/commonTypes"
 import { merchantOrder, preference, payment } from "lib/mercadopago"
 
-export async function getMerchantOrderId(orderData: MerchantOrderGetData) {
+export async function getMerchantOrderId(orderData: MerchantOrderGetData): Promise<MerchantOrderResponse> {
     try {
         const orderId = await merchantOrder.get(orderData)
         return orderId
@@ -13,7 +16,7 @@ export async function getMerchantOrderId(orderData: MerchantOrderGetData) {
     }
 }
 
-export async function createPreference(data: PreferenceCreateData) {
+export async function createPreference(data: PreferenceCreateData): Promise<PreferenceResponse> {
     try {
         const newPreference = await preference.create(data)
         return newPreference
@@ -23,7 +26,7 @@ export async function createPreference(data: PreferenceCreateData) {
     }
 }
 
-export async function getPayment(id: PaymentGetData) {
+export async function getPayment(id: PaymentGetData): Promise<PaymentResponse> {
     try {
         const paymentData = await payment.get(id)
         return paymentData

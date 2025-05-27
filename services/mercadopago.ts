5e878d0bcc88a1b3b81e96e2e8fd9ead6693789e
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-05-27 02:18:34 -0300

    delete - se elimino el endpoint para obtener una preferencia,ademas se elimino la funcion getPreference en services/mercadopago

--- services/mercadopago.ts
+++ services/mercadopago.ts
@@ -1,7 +1,6 @@
 import type { MerchantOrderGetData } from "mercadopago/dist/clients/merchantOrder/get/types"
 import { PaymentGetData } from "mercadopago/dist/clients/payment/get/types"
 import { PreferenceCreateData } from "mercadopago/dist/clients/preference/create/types"
-import { PreferenceGetData } from "mercadopago/dist/clients/preference/get/types"
 import { merchantOrder, preference, payment } from "lib/mercadopago"
 
 export async function getMerchantOrderId(orderData: MerchantOrderGetData) {
@@ -24,15 +23,6 @@ export async function createPreference(data: PreferenceCreateData) {
     }
 }
 
-export async function getPreference(id: PreferenceGetData) {
-    try {
-        const preferenceData = await preference.get(id)
-        return preferenceData
-    } catch (error) {
-        console.error("hubo un problema al obtener la preferencia:", error.message)
-        throw error
-    }
-}
 export async function getPayment(id: PaymentGetData) {
     try {
         const paymentData = await payment.get(id)

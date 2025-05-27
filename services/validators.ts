dfced45e588057c557882610a86e53210c7051c4
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-05-27 02:07:51 -0300

    refactor - se modificaron los parametros recibidos en las funciones validateAuth,validateAuthToken y validateUserUpdate, ademas se crearon las funciones validateCartQuery y validateCartBody para validar query y body de los endpoints relacionados a cart

--- services/validators.ts
+++ services/validators.ts
@@ -1,10 +1,10 @@
-import type { NextApiRequest } from "next"
 import { authSchema, tokenSchema } from "lib/schemas/authSchema"
 import { updateUserSchema } from "lib/schemas/userSchema"
+import { partialCartSchema } from "lib/schemas/cartSchema"
 
-export function validateAuth(req: NextApiRequest) {
+export function validateAuth(email: string): { email: string } {
     try {
-        const result = authSchema.safeParse(req)
+        const result = authSchema.safeParse(email)
         if (!result.success) {
             const errorMessage = result.error.issues.map((issue) => issue.message).join(";")
             throw new Error(errorMessage)
@@ -16,9 +16,9 @@ export function validateAuth(req: NextApiRequest) {
     }
 }
 
-export function validateAuthToken(req: NextApiRequest) {
+export function validateAuthToken(data: { email: string, code: number }): { email: string, code: number } {
     try {
-        const result = tokenSchema.safeParse(req)
+        const result = tokenSchema.safeParse(data)
         if (!result.success) {
             const errorMessage = result.error.issues.map((issue) => issue.message).join(";")
             throw new Error(errorMessage)
@@ -30,14 +30,54 @@ export function validateAuthToken(req: NextApiRequest) {
     }
 }
 
-export function validateUserUpdate(req: NextApiRequest){
+export function validateUserUpdate(data: UserData): {
+    email?: string;
+    userName?: string;
+    phoneNumber?: string;
+    address?: {
+        street?: string;
+        locality?: string;
+        city?: string;
+        state?: string;
+        postalCode?: number;
+        country?: string;
+    }
+} {
     try {
-        const result = updateUserSchema.safeParse(req)
+        const result = updateUserSchema.safeParse(data)
         if (!result.success) {
             const errorMessage = result.error.issues.map((issue) => issue.message).join(";")
             throw new Error(errorMessage)
         }
         return result.data
+    } catch (error) {
+        console.error(error.message)
+        throw error
+    }
+}
+
+export function validateCartQuery(productId:string): string {
+    try {
+        const result = partialCartSchema.safeParse({productId})
+        if (!result.success) {
+            const errorMessage = result.error.issues.map((issue) => issue.message).join(";")
+            throw new Error(errorMessage)
+        }
+        return result.data.productId
+    } catch (error) {
+        console.error(error.message)
+        throw error
+    }
+}
+
+export function validateCartBody(quantity: number): number {
+    try {
+        const result = partialCartSchema.safeParse({quantity})
+        if (!result.success) {
+            const errorMessage = result.error.issues.map((issue) => issue.message).join(";")
+            throw new Error(errorMessage)
+        }
+        return result.data.quantity
     } catch (error) {
         console.error(error.message)
         throw error

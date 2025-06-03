62670c49222c9bea1b2e0a6b6f9f46f071a127c5
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-06-02 22:32:17 -0300

    feat - se creo el searchSchema para validar los parametros de busqueda de productos

---
+++ lib/schemas/searchSchema.ts
@@ -0,0 +1,11 @@
+import { transform, z } from "zod/v4"
+
+const searchSchema = z.strictObject({
+    q: z.string(),
+    offset: z.preprocess((val) => Number(val), z.number().int().nonnegative()).transform((val) => String(val)),
+    limit: z.preprocess((val) => Number(val), z.number().int().nonnegative()).transform((val) => String(val)),
+    sort: z.enum(["price_asc", "price_desc"])
+})
+
+const partialSearchSchema = searchSchema.partial().strict()
+export { partialSearchSchema }

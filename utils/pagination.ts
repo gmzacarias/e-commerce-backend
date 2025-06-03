4f2b2ce67736f4e5856bdcdf8df4636ccb139e2d
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-06-02 23:19:35 -0300

    refactor - se modifico la logica  en la funcion getOffsetAndLimit para obtener los parametros limit y offset

--- utils/pagination.ts
+++ utils/pagination.ts
@@ -1,9 +1,8 @@
-import { NextApiRequest } from "next"
 
-export function getOffsetAndLimit(req: NextApiRequest, maxLimit = 100, maxOffset = 10000) {
-    const queryLimit = parseInt(req.query.limit as string ?? "10", 10)
-    const queryOffset = parseInt(req.query.offset as string ?? "0", 10)
-    const limit = Math.min(Math.max(queryLimit, 1), maxLimit)
+export function getOffsetAndLimit(newOffset: string, newLimit: string, maxLimit = 100, maxOffset = 10000) {
+    const queryOffset = parseInt(newOffset ?? "0", 10)
+    const queryLimit = parseInt(newLimit ?? "10", 10)
     const offset = Math.min(queryOffset, queryLimit)
+    const limit = Math.min(Math.max(queryLimit, 1), maxLimit)
     return { limit, offset }
 }

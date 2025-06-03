a127c143f267c8ec77511b9c7ff02790a02277da
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-06-02 23:53:13 -0300

    fix - se corrigio el import del metodo para validar productId

--- pages/api/product/[productId].ts
+++ pages/api/product/[productId].ts
@@ -1,13 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import method from "micro-method-router"
 import { handlerCORS } from "lib/corsMiddleware"
-import { validateSearchProduct } from "services/validators"
+import { validateProductId } from "services/validators"
 import { searchProductById } from "services/algolia"
 
 async function getHandler(req: NextApiRequest, res: NextApiResponse) {
 
     try {
-        const productId = validateSearchProduct(req.query.productId as string)
+        const productId = validateProductId(req.query.productId as string)
         const product = await searchProductById(productId)
         res.status(200).send(product)
     } catch (error) {

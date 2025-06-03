c1c3bf3f071b3c760c1c983699a0b9ccb354aaf0
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-06-02 23:50:29 -0300

    feat - se creo el orderIdSchema para validar los orderId

--- lib/schemas/orderSchema.ts
+++ lib/schemas/orderSchema.ts
@@ -1,7 +1,11 @@
 import { z } from "zod/v4"
 
 const orderSchema = z.strictObject({
-    additionalInfo: z.string().regex(/^[a-zA-Z0-9 ]+$/,"solo se permiten letras, números y espacios").optional()
+    additionalInfo: z.string().regex(/^[a-zA-Z0-9 ]+$/, "solo se permiten letras, números y espacios").optional()
 })
 
-export { orderSchema }
+const orderIdSchema = z.strictObject({
+    orderId: z.string().regex(/^[A-Za-z0-9]+$/, "solo se permiten mayusculas,minusculas y números").length(20, "el id debe tener exactamente 20 caracteres")
+})
+
+export { orderSchema, orderIdSchema }

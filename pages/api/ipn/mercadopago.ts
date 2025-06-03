638d5108b766e081434dfea1d4469876172685a9
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-06-02 21:06:08 -0300

    refactor - se modifico la forma de obtener los parametros de la query,ademas se modifico el condicional si no se encuentra un token valido

--- pages/api/ipn/mercadopago.ts
+++ pages/api/ipn/mercadopago.ts
@@ -1,17 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import method from "micro-method-router"
 import { handlerCORS } from "lib/corsMiddleware"
-import { handlePaidMerchantOrder } from "controllers/order"
+import { setPaymentOrder, updateStatusOrder } from "controllers/order"
 import { authMiddleware } from "lib/middleware"
 
 async function postHandler(req: NextApiRequest, res: NextApiResponse, token: { userId: string }) {
-    const topic = req.query.topic as string
-    const id = req.query.id as string
     try {
-        if (!token) {
-            throw new Error("no hay token")
+        if (!token.userId) {
+            throw new Error("token invalido o no autorizado")
         }
-        await handlePaidMerchantOrder(token.userId, topic, id)
+        await updateStatusOrder(token.userId, req.query.topic as string, req.query.id as string)
         res.status(200).send({ message: "Compra exitosa" })
     } catch (error) {
         if (error.message) {

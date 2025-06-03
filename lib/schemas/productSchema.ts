c7e3f43e26d40e220b460c6c44e48bf017ad8932
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2025-06-02 22:31:34 -0300

    feat - se creo el productSchema para validar la busqueda de productos por id

---
+++ lib/schemas/productSchema.ts
@@ -0,0 +1,7 @@
+import { transform, z } from "zod/v4"
+
+const productSchema = z.strictObject({
+    productId: z.preprocess((val) => Number(val), z.number().int().positive()).transform((val) => String(val))
+})
+
+export { productSchema }

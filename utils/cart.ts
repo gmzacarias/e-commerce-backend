6454304f734f88fff934ac43bca79c00a5eb49b0
gastonmzacarias <gastonmzacarias@gmail.com>
2025-06-20 00:37:54 -0300

    feat - se creo la funcion hasStock, para verificar el stock, antes de crear una roden

--- utils/cart.ts
+++ utils/cart.ts
@@ -62,3 +62,26 @@ export function formatItemsForPreference(data: ProductData[]): ItemsData[] {
         throw error
     }
 }
+
+export function hasStock(data: ProductData[]): ProductData[] {
+    try {
+        if (!hasProducts(data)) {
+            throw new Error("no hay productos")
+        }
+        const outOfStock = []
+        for (const i of data) {
+            const resta = i.stock - i.quantity
+            i.stock = resta
+            if (resta < 0) {
+                outOfStock.push(i.objectID)
+            }
+        }
+        if (outOfStock.length > 0) {
+            throw new Error(`productos sin stock:${outOfStock.join(", ")}`)
+        }
+        return data
+    } catch (error) {
+        console.error(error.message)
+        throw error
+    }
+}

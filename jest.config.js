488971cb3735f4e1a87b270c999b510436462052
gastonmzacarias <gastonmzacarias@gmail.com>
2025-06-28 23:46:47 -0300

    fix - se configuro  en jest.config,moduleNameMapper para resolver alias de tsconfig en tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'node', // o 'jsdom' si estás testeando componentes
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  testMatch: ['**/__tests__/**/*.(test|spec).ts?(x)', '**/?(*.)+(test|spec).ts?(x)'],
+setupFiles: ['<rootDir>/jest.setup.js']
 };
 
-process.env = Object.assign(process.env, {
-  JWT_SECRET:process.env.JWT_SECRET,
-});

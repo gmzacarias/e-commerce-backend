579a0b00529a07c62ed09515544a93e57be36682
gastonmzacarias <gastonmzacarias@gmail.com>
2025-06-28 23:47:18 -0300

    chore - se modificaron los imports para obtener las funciones para testear los tokens

--- lib/jwt.test.ts
+++ lib/jwt.test.ts
@@ -1,10 +1,10 @@
 import {expect, test} from '@jest/globals';
-import { generate,decode } from './jwt';
+import { generateToken,decodeToken } from 'services/jwt';
 
 test('jwt encode/decode', () => {
 const payload = {gaston:true}
-const token = generate(payload)
-const salida = decode(token)
+const token = generateToken(payload)
+const salida = decodeToken(token as string)
 delete (salida as { iat?: any }).iat;
   
     expect(payload).toEqual(salida)

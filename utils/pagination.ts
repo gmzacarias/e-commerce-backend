80181f790f16a66b0cc7937904815bec3ec64162
gastonmzacarias <gastonmzacarias@gmail.com>
2025-06-29 17:17:20 -0300

    fix - se corrigio la funcion getOffsetAndLimit, se añadio la verificacion en el caso de que offset y limit, vengan sin un valor definido

--- utils/pagination.ts
+++ utils/pagination.ts
@@ -1,8 +1,10 @@
 
-export function getOffsetAndLimit(newOffset: string, newLimit: string, maxLimit = 100, maxOffset = 10000) {
-    const queryOffset = parseInt(newOffset ?? "0", 10)
-    const queryLimit = parseInt(newLimit ?? "10", 10)
-    const offset = Math.min(queryOffset, queryLimit)
+export function getOffsetAndLimit(newOffset?: string, newLimit?: string, maxLimit = 100, maxOffset = 10000) {
+    const rawOffset = parseInt(newOffset ?? "", 10)
+    const rawLimit = parseInt(newLimit ?? "", 10)
+    const queryOffset = isNaN(rawOffset) ? 0 : rawOffset;
+    const queryLimit = isNaN(rawLimit) ? 10 : rawLimit
+    const offset = Math.min(Math.max(queryOffset, 0), maxOffset)
     const limit = Math.min(Math.max(queryLimit, 1), maxLimit)
     return { limit, offset }
 }

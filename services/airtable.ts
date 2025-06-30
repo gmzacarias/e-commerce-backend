beb44ea85e94e6da471217a2f5cf40a52bfbde17
gastonmzacarias <gastonmzacarias@gmail.com>
2025-06-30 18:05:39 -0300

    refactor - se modifico el error al no obtener una respuesta ok desde el fetch,ademas se agregaron condicionales, si no hay records disponibles

--- services/airtable.ts
+++ services/airtable.ts
@@ -2,7 +2,7 @@ import { airTableConfig } from "lib/airtable"
 
 const { baseId, tableName, token } = airTableConfig
 
-export async function authAirtable():Promise <AirtableData[]> {
+export async function authAirtable(): Promise<AirtableData[]> {
     try {
         const response = await fetch(`https://api.airtable.com/v0/${baseId}/${tableName}/listRecords`, {
             method: "POST",
@@ -13,13 +13,13 @@ export async function authAirtable():Promise <AirtableData[]> {
         }
         )
         if (!response.ok) {
-            throw new Error("hubo un error al obtener los datos")
+            throw new Error("La respuesta de Airtable no fue exitosa")
         }
-        const data = await response.json();
-        const dataRecords= data.records.map((record: { fields: AirtableData }) => record.fields);
-        if (!dataRecords || dataRecords.length === 0) {
+        const data = await response.json()
+        if (!data || !data.records || !Array.isArray(data.records) || data.records.length === 0) {
             throw new Error("No hay records disponibles")
         }
+        const dataRecords = data.records.map((record: { fields: AirtableData }) => record.fields)
         return dataRecords
     } catch (error) {
         console.error("Hubo un Error al obtener los datos", error.message)

e4e048745e67953f022d90759febafff450b6106
gastonmzacarias <gastonmzacarias@gmail.com>
2025-06-30 18:24:37 -0300

    feat - se creo el test para validar la funcion de obtener un codigo generado por randomSeed

---
+++ services/randomSeed.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect } from "@jest/globals"
+import { generateRandomCode } from "./randomSeed"
+
+describe("test in randomSeed", () => {
+    it("should return a number of five digits between 10000 and 99999", () => {
+        const code = generateRandomCode()
+        expect(typeof code).toBe("number")
+        expect(code).toBeGreaterThanOrEqual(10000)
+        expect(code).toBeLessThanOrEqual(99999)
+    })
+})

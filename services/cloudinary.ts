347bba6e94ea744f7ae3072c38c4da0d90cbb936
gastonmzacarias <gastonmzacarias@gmail.com>
2025-06-30 19:48:20 -0300

    refactor - se elimino el error si no habia una respuesta al subir una imagen,en el caso de un error,lo captura el catch de la funcion.

--- services/cloudinary.ts
+++ services/cloudinary.ts
@@ -9,9 +9,6 @@ export async function uploadCloudinary(image: string): Promise<UploadApiResponse
                 discard_original_filename: true,
                 format: "webp"
             })
-        if (!response) {
-            throw new Error("hubo un error al subir las imagenes")
-        }
         return response
     } catch (error) {
         console.error("Error en Cloudinary:", error.message)

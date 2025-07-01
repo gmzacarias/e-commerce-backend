2393483ba77ad6b16943a6868e1ba2c4f791f3e2
gastonmzacarias <gastonmzacarias@gmail.com>
2025-06-30 21:39:29 -0300

    feat - se creo el test para validar un metodo de obtener datos de un usuario y en el metodo actualizar los datos del usuario en la clase UserService

---
+++ services/user.test.ts
@@ -0,0 +1,103 @@
+import { describe, expect } from "@jest/globals"
+import { UserService } from "./user"
+
+describe("test in user service", () => {
+    let userService: UserService
+    let mockUserRepo: any
+    let mockAuthRepo: any
+
+    beforeEach(() => {
+        mockUserRepo = {
+            getUser: jest.fn(),
+            save: jest.fn(),
+        }
+
+        mockAuthRepo = {
+            getAuth: jest.fn(),
+            save: jest.fn()
+        }
+        userService = new UserService(mockUserRepo, mockAuthRepo)
+    })
+
+    it("should returns user data", async () => {
+        const mockUser = { data: { userName: "John", email: "john@email.com", phoneNumber: "123", address: { city: "my city" } } }
+        mockUserRepo.getUser.mockResolvedValue(mockUser)
+        const result = await userService.getUserData("user123")
+        expect(mockUserRepo.getUser).toHaveBeenCalledWith("user123")
+        expect(result).toEqual(mockUser.data)
+    })
+
+    it("should updates and saves user and auth", async () => {
+        const mockUser = {
+            data: {
+                userName: "oldName",
+                email: "oldEmail@email.com",
+                phoneNumber: "123",
+                address: {
+                    city: "oldCity"
+                }
+
+            },
+            updateUserName: jest.fn(),
+            updateEmail: jest.fn(),
+            updatePhoneNumber: jest.fn(),
+            updateAddress: jest.fn()
+        }
+
+        const mockAuth = {
+            updateEmail: jest.fn()
+        }
+        mockUserRepo.getUser.mockResolvedValue(mockUser)
+        mockAuthRepo.getAuth.mockResolvedValue(mockAuth)
+        mockUserRepo.save.mockResolvedValue(true)
+        mockAuthRepo.save.mockResolvedValue(true)
+
+        const updateData = {
+            userName: "newName",
+            email: "newEmail@email.com",
+            phoneNumber: "1234567890",
+            address: {
+                city: "new city"
+            }
+        }
+
+        const result = await userService.updateUserData("user123", updateData)
+        expect(mockUserRepo.getUser).toHaveBeenCalledWith("user123")
+        expect(mockAuthRepo.getAuth).toHaveBeenCalledWith("user123")
+        expect(mockUser.updateUserName).toHaveBeenCalledWith(updateData.userName)
+        expect(mockUser.updateEmail).toHaveBeenCalledWith(updateData.email)
+        expect(mockAuth.updateEmail).toHaveBeenCalledWith(updateData.email)
+        expect(mockUser.updatePhoneNumber).toHaveBeenCalledWith(updateData.phoneNumber)
+        expect(mockUser.updateAddress).toHaveBeenCalledWith(updateData.address)
+        expect(mockUserRepo.save).toHaveBeenCalledWith(mockUser)
+        expect(mockAuthRepo.save).toHaveBeenCalledWith(mockAuth)
+        expect(result).toEqual(mockUser.data)
+    })
+
+    it("should throw an error if getuser data fails", async () => {
+        const error = new Error("No existe informacion relacionada al id ingresado")
+        mockUserRepo.getUser.mockRejectedValue(error)
+        await expect(userService.getUserData("user123")).rejects.toThrow(error)
+    })
+
+    it("should throw an error if save fails", async () => {
+        const error = new Error("Error al actualizar los datos")
+        const mockUser = {
+            data: {},
+            updateUserName: jest.fn(),
+            updateEmail: jest.fn(),
+            updatePhoneNumber: jest.fn(),
+            updateAddress: jest.fn()
+        }
+
+        const mockAuth = {
+            updateEmail: jest.fn()
+        }
+        mockUserRepo.getUser.mockResolvedValue(mockUser)
+        mockAuthRepo.getAuth.mockResolvedValue(mockAuth)
+        mockUserRepo.save.mockRejectedValue(error)
+        mockAuthRepo.save.mockRejectedValue(error)
+        const updateData = { email: "failEmail@email.com" }
+        await expect(userService.updateUserData("user123", updateData)).rejects.toThrow(error)
+    })
+})

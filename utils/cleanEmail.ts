c438925b1ddcee74eb793f2ebfca10cad775745c
gastonmzacarias <gastonmzacarias@gmail.com>
2025-07-08 17:46:46 -0300

    refactor - se modifico la logica en la funcion cleanEmail,se añadio try/catch para controlar los errores ,y se añadio un condicional si el parametro recibido no es del type string

--- utils/cleanEmail.ts
+++ utils/cleanEmail.ts
@@ -1,3 +1,11 @@
 export function cleanEmail(email: string): string {
-    return email.trim().toLowerCase()
+    try {
+        if (typeof email !== "string") {
+            throw new Error(`El type de ${email} no es de type string`)
+        }
+        return email.trim().toLowerCase()
+    } catch (error) {
+        console.error("Hubo un error al normalizar el email:", error.message)
+        throw error
+    }
 }

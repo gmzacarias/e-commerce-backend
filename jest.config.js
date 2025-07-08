dde7ea081ff2b809f702efce398025708b439989
gastonmzacarias <gastonmzacarias@gmail.com>
2025-07-07 21:43:35 -0300

    chore - se modifico la configuracion en jest para poder leer archivos .tsx

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,19 @@ const { compilerOptions } = require('./tsconfig.json');
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node', // o 'jsdom' si estás testeando componentes
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest', 
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
     prefix: '<rootDir>/',
   }),
   moduleDirectories: ['node_modules', '<rootDir>'],
-  testMatch: ['**/__tests__/**/*.(test|spec).ts?(x)', '**/?(*.)+(test|spec).ts?(x)'],
-setupFiles: ['<rootDir>/jest.setup.js']
+  testMatch: [
+    '**/__tests__/**/*.(test|spec).ts?(x)',
+    '**/?(*.)+(test|spec).ts?(x)',
+    '**/test/**/*.test.ts?(x)',
+  ],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
-

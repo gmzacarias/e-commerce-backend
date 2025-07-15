fc93ce410cb82a53f59a38c4ebc5df64aee9a0f2
gastonmzacarias <gastonmzacarias@gmail.com>
2025-07-15 20:15:05 -0300

    fix - se añadieron los test para chequear el metodo getMerchantOrderById,se incluyen test en el caso de que ocurrieran errores

---
+++ test/services/mercadoPago/getMerchantOrderId.test.ts
@@ -0,0 +1,52 @@
+import { describe, expect } from "@jest/globals"
+import { merchantOrder } from "lib/mercadopago";
+import { getMerchantOrderId } from "services/mercadopago"
+
+jest.mock("lib/mercadopago", () => ({
+    merchantOrder: {
+        get: jest.fn(),
+    },
+}))
+
+describe("test in method getMerchantOrderById", () => {
+    it("should return the order data associated with the id", async () => {
+        const orderData = {
+            merchantOrderId: 12458
+        };
+        const mockOrder = {
+            id: 123458,
+            status: "closed",
+            external_reference: "default",
+            preference_id: "Preference identification",
+            date_created: "2018-09-14T17:11:31.000Z",
+            items: [
+                {
+                    id: 1234,
+                    category_id: "item category",
+                    currency_id: "BRL",
+                    description: "item description",
+                    picture_url: "item picture url",
+                    title: "item title",
+                    quantity: 1,
+                    unit_price: 5
+                }
+            ],
+
+        };
+        (merchantOrder.get as jest.Mock).mockResolvedValue(mockOrder);
+        const result = await getMerchantOrderId(orderData);
+        expect(merchantOrder.get).toHaveBeenCalledWith(orderData);
+        expect(result).toEqual(mockOrder);
+    })
+
+    it("should throw an error when getmerchantorderbyid does not find the order associated with the Id", async () => {
+        const error = new Error("No se encontra una orden relacionada al Id")
+        const orderData = {
+            merchantOrderId: 12458
+        };
+
+        (merchantOrder.get as jest.Mock).mockRejectedValue(error);
+        await expect(getMerchantOrderId(orderData)).rejects.toThrow(error);
+        expect(merchantOrder.get).toHaveBeenCalledWith(orderData);
+    })
+})

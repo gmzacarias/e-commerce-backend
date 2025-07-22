8d916413b65f6ff20d97a767362aede7697b1000
gastonmzacarias <gastonmzacarias@gmail.com>
2025-07-22 19:08:01 -0300

    fix - se añadieron los test para chequear en la funcion generateRandomCode,se incluyen test en el caso de que ocurrieran errores

---
+++ test/services/randomSeed/generateRandomCode.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect } from "@jest/globals"
+import { generateRandomCode } from "services/randomSeed"
+import gen from "random-seed"
+
+describe("test in function generateRandomCode", () => {
+    it("should return a number of five digits between 10000 and 99999", () => {
+        const code = generateRandomCode();
+        expect(typeof code).toBe("number");
+        expect(code).toBeGreaterThanOrEqual(10000);
+        expect(code).toBeLessThanOrEqual(99999);
+    })
+
+    it("should throw an error when randomSeed.create fails", () => {
+        const error = new Error("Hubo un error al generar el codigo random");
+        jest.spyOn(gen, "create").mockImplementation(() => {
+            throw error;
+        });
+        expect(() => generateRandomCode()).toThrow(error);
+    })
+})

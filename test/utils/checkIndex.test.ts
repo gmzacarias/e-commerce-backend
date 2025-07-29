77bb92f6e1ea893d03e63edb94a49b61f9c114ed
gastonmzacarias <gastonmzacarias@gmail.com>
2025-07-29 01:47:00 -0300

    fix - se añadieron los test para chequear en la funcion checkIndex,se incluyen test en el caso de que ocurrieran errores

--- test/utils/checkIndex.test.ts
+++ test/utils/checkIndex.test.ts
@@ -1,20 +1,20 @@
 import { describe, expect } from "@jest/globals"
-import { checkIndexAlgolia } from "services/algolia"
+import { checkIndex } from "utils/checkIndex"
 import { productIndex, productsAscIndex, productsDescIndex } from "lib/algolia"
 
 describe("test in function checkIndexAlgolia", () => {
     it("should return productsAscIndex if sort is equal to price_asc", () => {
-        const result = checkIndexAlgolia("price_asc")
+        const result = checkIndex("price_asc")
         expect(result).toBe(productsAscIndex)
     })
 
     it("should return productsAscIndex if sort is equal to price_desc", () => {
-        const result = checkIndexAlgolia("price_desc")
+        const result = checkIndex("price_desc")
         expect(result).toBe(productsDescIndex)
     })
 
     it("should return productIndex if sort is not equal to price_asc or price_desc", () => {
-        const result = checkIndexAlgolia("")
+        const result = checkIndex("")
         expect(result).toBe(productIndex)
     })
 })

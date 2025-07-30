f0faba18cfd2fa019d77589ed9b224eefa6b17b6
gastonmzacarias <gastonmzacarias@gmail.com>
2025-07-29 21:36:41 -0300

    chore - se añadieron export a las funciones sendSaleConfirmed & sendPaymentConfirmed ,ademas se eliminaron las funciones purchaseAlert & saleAlert

--- services/sendgrid.ts
+++ services/sendgrid.ts
@@ -8,7 +8,7 @@ export async function sendCodeAuth(email: string, code: number) {
     const mailHtml = render(sendCodeMail({ code: code }))
     const msg = {
         to: email,
-        from: sender, // Use the email address or domain you verified above
+        from: sender,
         subject: '🔐 Código de verificación para iniciar sesión en Smartshop',
         html: mailHtml,
     };
@@ -19,11 +19,11 @@ export async function sendCodeAuth(email: string, code: number) {
     }
 }
 
-async function sendPaymentConfirmed(email: string, userName: string, order: OrderData) {
+export async function sendPaymentConfirmed(email: string, userName: string, order: OrderData) {
     const mailHtml = render(paymentMail({ userName, order }))
     const msg = {
         to: email,
-        from: sender, // Use the email address or domain you verified above
+        from: sender,
         subject: `✅ Confirmación de tu compra en Smartshop - Orden #${order.orderId}`,
         html: mailHtml,
     };
@@ -34,11 +34,11 @@ async function sendPaymentConfirmed(email: string, userName: string, order: Orde
     }
 }
 
-async function sendSaleConfirmed(user:UserData, order:OrderData) {
+export async function sendSaleConfirmed(user:UserData, order:OrderData) {
     const mailHtml = render(saleMail({ user,order }))
     const msg = {
         to: adminEmail,
-        from: sender, // Use the email address or domain you verified above
+        from: sender,
         subject:`✅ Confirmación de venta en Smartshop - Orden #${order.orderId}`,
         html: mailHtml,
     };
@@ -46,23 +46,5 @@ async function sendSaleConfirmed(user:UserData, order:OrderData) {
         await sgMail.send(msg)
     } catch (error) {
         console.error("Email no enviado", error.message)
-    }
-}
-
-export async function purchaseAlert(email: string, userName: string, order: OrderData) {
-    try {
-        const data = await sendPaymentConfirmed(email, userName, order)
-        return data
-    } catch (error) {
-        console.error("No se pudo enviar el mail ", error.message)
-    }
-}
-
-export async function saleAlert(user:UserData,order:OrderData) {
-    try {
-        const data = await sendSaleConfirmed(user, order)
-        return data
-    } catch (error) {
-        console.error("No se pudo enviar el mail ", error.message)
     }
 }

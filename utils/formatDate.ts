850e3913d4235f89e704fc27a20ed0e36a363af6
gastonmzacarias <gastonmzacarias@gmail.com>
2025-07-30 22:29:21 -0300

    refactor - se añadio un error en el caso de que los datos de timestamp no sean validos

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,6 +1,9 @@
 export function formatDate(date: FirestoreTimestamp): Date {
     try {
         const { _nanoseconds, _seconds } = date
+        if (typeof _seconds !== "number" || typeof _nanoseconds !== "number") {
+            throw new Error("Los datos no son del type number");
+        }
         const newDate = new Date(_seconds * 1000 + Math.floor(_nanoseconds / 1000000))
         return newDate
     } catch (error) {

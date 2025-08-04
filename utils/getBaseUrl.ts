47beaa4910944031a3765e684c6aae6d44f0798e
gastonmzacarias <gastonmzacarias@gmail.com>
2025-08-03 23:14:33 -0300

    refactor - se añadieron condicionales en el caso de que Hookdeck y/o Vercel no esten definidos

--- utils/getBaseUrl.ts
+++ utils/getBaseUrl.ts
@@ -3,6 +3,8 @@ export function getBaseUrl(): UrlData {
         const isDev = process.env.NODE_ENV === "development"
         const baseUrl = process.env.HOOKDECK_URL
         const productionUrl = process.env.VERCEL_URL
+        if (!baseUrl) throw new Error("baseUrl no esta definida")
+        if (!productionUrl) throw new Error("productionUrl no esta definida")
         const notificationUrl = isDev ? baseUrl : productionUrl
         const successUrl = baseUrl
         const pendingUrl = baseUrl

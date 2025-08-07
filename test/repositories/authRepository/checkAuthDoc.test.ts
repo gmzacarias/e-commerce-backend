1daf262116ca9bd3f95274d7bdca2f4c9840d9e3
gastonmzacarias <gastonmzacarias@gmail.com>
2025-08-07 02:23:07 -0300

    refactor - se modifico la logica para mockear el repo y sus metodos internos

--- test/repositories/authRepository/checkAuthDoc.test.ts
+++ test/repositories/authRepository/checkAuthDoc.test.ts
@@ -1,41 +1,60 @@
 import { AuthRepository } from "repositories/authRepository"
 
 describe("test in method checkAuthDoc", () => {
-    const mockDoc = (exists: boolean, userId?: string) => ({
-        exists,
-        data: () => ({ userId }),
-    });
-
-    const setupRepoWithMockDoc = (mockedDoc: any): any => {
+    const userId = "userId001";
+    const authData = {
+        userId: userId,
+        email: "test@example.com",
+        code: 123456,
+    };
+   
+    it("should return true if the user and document id are associated", async () => {
+        const mockGetDoc = {
+            exists: true,
+            data: () => authData,
+        };
         const repo = new AuthRepository() as any
         repo.authCollection = {
             doc: jest.fn().mockReturnValue({
-                get: jest.fn().mockResolvedValue(mockedDoc),
+                get: jest.fn().mockResolvedValue(mockGetDoc),
             }),
-        }
-        return repo
-    };
-
-    it("should return true if the user and document id are associated", async () => {
-        const repo = setupRepoWithMockDoc(mockDoc(true, "user123"));
-        const result = await repo.checkAuthDoc("docId1234", "user123");
+        };
+        const result = await repo.checkAuthDoc("docId1234", userId);
         expect(result).toBe(true);
     })
 
     it("should throw an error when the document does not exist", async () => {
         const error = new Error("no existe un documento asociado a este id");
-        const repo = setupRepoWithMockDoc({ exists: false });
+        const mockGetDoc = {
+            exists: false,
+            data: () => { },
+        };
+        const repo = new AuthRepository() as any
+        repo.authCollection = {
+            doc: jest.fn().mockReturnValue({
+                get: jest.fn().mockResolvedValue(mockGetDoc),
+            }),
+        };
         const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => { });
-        await expect(repo.checkAuthDoc("docId1234", "user123")).rejects.toThrow(error);
+        await expect(repo.checkAuthDoc("docId1234", userId)).rejects.toThrow(error);
         expect(consoleSpy).toHaveBeenCalledWith("no se pudo obtener el documento:", error.message);
         consoleSpy.mockRestore();
     })
 
     it("should throw an error when userId does not match", async () => {
         const error = new Error("el usuario no tiene acceso");
-        const repo = setupRepoWithMockDoc(mockDoc(true, "user1234"));
+        const mockGetDoc = {
+            exists: true,
+            data: () => authData,
+        };
+        const repo = new AuthRepository() as any
+        repo.authCollection = {
+            doc: jest.fn().mockReturnValue({
+                get: jest.fn().mockResolvedValue(mockGetDoc),
+            }),
+        };
         const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => { });
-        await expect(repo.checkAuthDoc("docId1234", "user1235")).rejects.toThrow(error);
+        await expect(repo.checkAuthDoc("docId1234", "userId003")).rejects.toThrow(error);
         expect(consoleSpy).toHaveBeenCalledWith("no se pudo obtener el documento:", error.message);
         consoleSpy.mockRestore();
     })

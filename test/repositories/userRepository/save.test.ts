8396ede11ccdd8c6a364a4b3380662d6d256e879
gastonmzacarias <gastonmzacarias@gmail.com>
2025-09-03 02:18:19 -0300

    fix - se añadieron los test para chequear el metodo save

---
+++ test/repositories/userRepository/save.test.ts
@@ -0,0 +1,51 @@
+import { UserRepository } from "repositories/userRepository"
+import { User } from "models/user"
+
+describe("test in method getCart", () => {
+    it("should update the order with the data received as parameters", async () => {
+        const userData = [{
+            id: "userId001",
+            data: {
+                userName: "userTest",
+                email: "userTest@email.com",
+                phoneNumber: "1234567890",
+                cart: [],
+                address: {}
+            }
+        }];
+        const repo = new UserRepository() as any;
+        const getUserDocSpy = jest.spyOn(repo, "getUserDoc").mockResolvedValue(true);
+        const updateMock = jest.fn().mockResolvedValue(true);
+        repo.userCollection = {
+            doc: jest.fn().mockReturnValue({
+                update: updateMock
+            })
+        };
+        const data = new User(userData[0].id, userData[0].data as any);
+        const result = await repo.save(data);
+        expect(getUserDocSpy).toHaveBeenCalledWith("userId001");
+        expect(updateMock).toHaveBeenCalledWith(data.data);
+        expect(result).toBe(true);
+    })
+
+    it("should throw an error when the document does not exist", async () => {
+        const error = new Error("no existe un documento asociado a este id");
+        const userData = [{
+            id: "userId001",
+            data: {
+                userName: "userTest",
+                email: "userTest@email.com",
+                phoneNumber: "1234567890",
+                cart: [],
+                address: {}
+            }
+        }];
+        const repo = new UserRepository as any;
+        const getUserDocSpy = jest.spyOn(repo, "getUserDoc").mockRejectedValue(error);
+        const consoleSpy = jest.spyOn(console, "error").mockImplementation(() => { });
+        await expect(repo.save(userData[0])).rejects.toThrow(error);
+        expect(getUserDocSpy).toHaveBeenCalledWith(userData[0].id);
+        expect(consoleSpy).toHaveBeenCalledWith("no se pudo actualizar el documento:", error.message);
+        consoleSpy.mockRestore();
+    })
+})

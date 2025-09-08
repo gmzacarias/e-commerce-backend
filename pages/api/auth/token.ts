7c5a1461558a2493e3e859a66d18f21572cffd31
gastonmzacarias <gastonmzacarias@gmail.com>
2025-09-07 21:55:59 -0300

    refactor - se refactorizo el endpoint para poder setear una nueva cookie con el valor de refreshToken

--- pages/api/auth/token.ts
+++ pages/api/auth/token.ts
@@ -3,12 +3,20 @@ import method from "micro-method-router"
 import { handlerCORS } from "lib/corsMiddleware"
 import { signIn } from "controllers/auth"
 import { validateAuthToken } from "services/validators"
+import { serialize } from "cookie"
 
 async function postHandler(req: NextApiRequest, res: NextApiResponse) {
     try {
         const tokenSchema = validateAuthToken(req.body)
         const { email, code } = tokenSchema
-        const token = await signIn(email, code)
+        const { token, refreshToken } = await signIn(email, code)
+        res.setHeader("Set-Cookie", serialize("refresh_token", refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            path: "/",
+            maxAge: 60 * 60 * 24 * 7,
+        }))
         res.status(200).send({ message: "inicio de sesion correcto", token: token })
     } catch (error) {
         if (error.message) {

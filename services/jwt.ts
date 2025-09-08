a05d2c502be4556155409ee29f4762589c1bb1e2
gastonmzacarias <gastonmzacarias@gmail.com>
2025-09-07 22:03:39 -0300

    refactor - se modifico la funcion generateToken, se añadio la opcion de expiracion, ademas se creo la funcion generateRefreshToken para poder generar un token para setearlo en una cookie

--- services/jwt.ts
+++ services/jwt.ts
@@ -1,11 +1,11 @@
 import { jwt, secret } from "lib/jwt"
 
-export function generateToken(data: string | object | Buffer): string | jwt.JwtPayload {
+export function generateToken(data: string | object | Buffer): string {
     try {
         if (!data || !secret) {
             throw new Error("faltan parametros para poder generar el token")
         }
-        const token = jwt.sign(data, secret)
+        const token = jwt.sign(data, secret, { expiresIn: "15m" })
         return token
     } catch (error) {
         console.error("Error en JWT:", error.message)
@@ -13,6 +13,18 @@ export function generateToken(data: string | object | Buffer): string | jwt.JwtP
     }
 }
 
+export function generateRefreshToken(data: string | object | Buffer): string {
+    try {
+        if (!data || !secret) {
+            throw new Error("faltan parametros para poder generar el token")
+        }
+        const refreshToken = jwt.sign(data, secret, { expiresIn: "7d" })
+        return refreshToken
+    } catch (error) {
+        console.error("Error en JWT:", error.message)
+        throw error
+    }
+}
 export function verifyToken(token: string): string | jwt.JwtPayload {
     try {
         if (!token || !secret) {

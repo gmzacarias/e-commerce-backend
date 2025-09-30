73c570187b85b4fdde817db78fdf900c8ed11739
gastonmzacarias <gastonmzacarias@gmail.com>
2025-09-30 20:23:17 -0300

    refactor - se refactorizo la funcion handlerCors,se eliminaron urls que hacian peticiones

--- lib/corsMiddleware.ts
+++ lib/corsMiddleware.ts
@@ -2,13 +2,19 @@ import { NextApiRequest, NextApiResponse } from "next";
 import NextCors from "nextjs-cors";
 
 export function handlerCORS(callback: { (req: NextApiRequest, res: NextApiResponse): void; (req: NextApiRequest, res: NextApiResponse): void; (req: NextApiRequest, res: NextApiResponse): void; (req: NextApiRequest, res: NextApiResponse): void; (req: NextApiRequest, res: NextApiResponse): void; (req: NextApiRequest, res: NextApiResponse): void; (arg0: NextApiRequest, arg1: NextApiResponse): void; }) {
-    return async function (req: NextApiRequest, res: NextApiResponse) {
-      await NextCors(req, res, {
-        methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE","OPTIONS"],
-        origin: ["http://localhost:3000","https://localhost:3000","https://e-commerce-smartshop.vercel.app","https://e-commerce-smartshop-api-docs.vercel.app"],
-        credentials:true,
-        optionsSuccessStatus: 200,
-      })
-      callback(req, res);
-    }
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://e-commerce-smartshop.vercel.app",
+    "https://e-commerce-smartshop-api-docs.vercel.app",
+  ];
+
+  return async function (req: NextApiRequest, res: NextApiResponse) {
+    await NextCors(req, res, {
+      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+      origin: allowedOrigins,
+      credentials: true,
+      optionsSuccessStatus: 200,
+    })
+    callback(req, res);
+  }
 }

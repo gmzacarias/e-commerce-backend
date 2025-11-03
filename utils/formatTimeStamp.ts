00e5084c02d4beb1cbc63e9e7a0b521f1691f2ab
gastonmzacarias <gastonmzacarias@gmail.com>
2025-11-02 21:35:53 -0300

    refactor - se modifico la logica de la funcion , para manejar el tipo de dato que recibe y como de volverlo formateado

---
+++ utils/formatTimeStamp.ts
@@ -0,0 +1,16 @@
+export function formatTimestamp(date: FirestoreTimestamp | Date): Date {
+    try {
+        if (date instanceof Date) {
+            return date
+        }
+        const { _nanoseconds, _seconds } = date
+        if (typeof _seconds !== "number" || typeof _nanoseconds !== "number") {
+            throw new Error("Los datos no son del type number");
+        }
+        const newDate = new Date(_seconds * 1000 + Math.floor(_nanoseconds / 1000000))
+        return newDate
+    } catch (error) {
+        console.error("no se pudo formatear la fecha :", error.message)
+        throw error
+    }
+}
